69ffb4bbf14eaa460ad78ca2cd9d4e11b8bfd1e0
Isaac Bowyer <c0007135@my.shu.ac.uk>
2023-11-27 13:35:32 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to functional_tasks
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/functional_tasks/core_test.clj
@@ -0,0 +1,7 @@
+(ns functional-tasks.core-test
+  (:require [clojure.test :refer :all]
+            [functional-tasks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
