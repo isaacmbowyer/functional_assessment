12343cee9d7fafa6ff764b50b6e109a9ff332af5
Isaac Bowyer <c0007135@my.shu.ac.uk>
2023-11-28 09:48:06 +0000

    Added conversion functions

--- src/functional_tasks/core.clj
+++ src/functional_tasks/core.clj
@@ -1,6 +1,20 @@
-(ns functional-tasks.core)
+(ns functional-tasks.core
+  (:require [clojure.spec.alpha :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; TASK 1
+(s/def ::valid-trinary
+  (s/and string?
+         #(re-matches #"^[012]*$" %)))
+
+(defn convert-string-to-list [number]
+  (reverse (map #(Character/getNumericValue ^char %) number)))
+
+(defn convert-trinary-to-decimal [number]
+  {:pre [(s/valid? ::valid-trinary number)]}
+  (loop [trinary-number-list (convert-string-to-list number)
+         decimal-number 0]
+
+  )
+)
+
+(defn -main [] ())
